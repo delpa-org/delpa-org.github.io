30eee6a193824887433f72f0a9bee413981744aa
Hong Xu <hong@topbug.net>
2025-01-05 17:27:24 -0800

    Show snapshot versions on homepage (#7)

--- README.md
+++ README.md
@@ -1,25 +1,33 @@
 # Delpa Website
 
-This website is built using [Docusaurus](https://docusaurus.io/), a modern static website generator.
+This website, located at https://delpa.org, is built using
+[Docusaurus](https://docusaurus.io/), a modern static website generator.
 
-### Install Dependencies
+## Install Dependencies
 
 ```console
 $ npm install
 ```
 
-### Local Development
+## Local Development
 
 ```console
 $ npm run dev
 ```
 
-This command starts a local development server and opens up a browser window. Most changes are reflected live without having to restart the server.
+This command starts a local development server and opens up a browser window.
+Most changes are reflected live without having to restart the server.
 
-### Build
+## Build
 
 ```
 $ npm run build
 ```
 
-This command generates static content into the `build` directory and can be served using any static contents hosting service.
+This command generates static content into the `build` directory and can be
+served using any static contents hosting service.
+
+## Special Notes
+
+The file `static/snapshot_versions.json` is deliberated set to wrong values. It
+will be overridden to correct values during deployment.
---
+++ scripts/gen_snapshot_versions_table.ts
@@ -0,0 +1,38 @@
+/** @license AGPL-3.0-or-later
+ *
+ * Copyright(C) 2025 Hong Xu <hong@topbug.net>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/** Generate the snapshot table from snapshot_versions.json. */
+
+import fs from "fs";
+import snapshotVersions from "../static/snapshot_versions.json" with { type: "json" };
+
+const table =
+  `
+| Date | URL |
+|------|-----|
+` +
+  snapshotVersions
+    .sort()
+    .reverse()
+    .map(
+      (date: string) =>
+        `| ${date} | https://packages.delpa.org/snapshot/${date}/ |`,
+    )
+    .join("\n");
+
+fs.writeFileSync("src/pages/_snapshot_versions.mdx", table);
